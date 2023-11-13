79b6d6ef435cc03aed12170a96164f8ab425039a
SAIRAM BOTLA <sairambotla0416@gmail.com>
2023-11-13 16:09:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'Del'){
+        string = string.substring(0,string.length-1)
+        console.log(string)
+        document.querySelector('input').value = string;
+      }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
+
